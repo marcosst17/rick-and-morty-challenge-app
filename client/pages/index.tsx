1c81473b103e790d8efa6e3854ea98b1d3a2a765
marcosst17 <stricker.marcos17@gmail.com>
2023-09-22 15:49:47 -0300

    Initialized frontend

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import axios from '@/node_modules/axios/index'
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+  return (
+    <div>Well, here we go</div>
+  )
+}
+
+export default App
