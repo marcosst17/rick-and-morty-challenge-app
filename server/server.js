72665f0b61e54f127cdbc5b093100e95e2951ef5
marcosst17 <stricker.marcos17@gmail.com>
2023-09-27 14:00:38 -0300

    Port test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express()
 const cors = require("cors")
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const mongoUri = process.env.DB_CONNECTION_STRING
 const mongoClient = new MongoClient(mongoUri);
 
